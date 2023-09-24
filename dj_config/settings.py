aa6fe4c2ec0af73f45aac4b6e44ba071e2aa1b99
ddahan <david.dahan3@gmail.com>
2023-09-24 23:12:31 +0000

    Clean repository

--- dj_config/settings.py
+++ dj_config/settings.py
@@ -50,13 +50,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",  # to handle CORS with right headers
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
